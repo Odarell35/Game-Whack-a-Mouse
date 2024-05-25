34f4c4b626c0488c4454f440c3cb0e8b3b54c112
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-05-25 20:32:04 +0200

    Desiging gameboard page

--- whack-a-mouse/src/mouse.js
+++ whack-a-mouse/src/mouse.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Mouse.css';
+import mice from './Img/mice.png';
 
 const Mouse = ({ index, isMouse, handleMouseClick }) => {
   return (
     <div className="mouse-hole" onClick={() => handleMouseClick(index)}>
-      {isMouse && <div className="mouse">💖</div>}
+      {isMouse && <div className="mouse">
+      <img className="Mouse-pawn" src={mice} alt='gamemice'/>
+        </div>}
     </div>
   );
 };
