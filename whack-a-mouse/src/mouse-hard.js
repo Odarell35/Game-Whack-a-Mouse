80f018aced2421536de79ba1ecf279350f07d5d3
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-06-01 15:31:30 +0200

    Update mouse-hard.js

--- whack-a-mouse/src/mouse-hard.js
+++ whack-a-mouse/src/mouse-hard.js
@@ -5,7 +5,7 @@ import mice from './Img/mice.png';
 const Mouse = ({ index, isMouse, handleMouseClick }) => {
   return (
     <div className="mouse-holeh" onClick={() => handleMouseClick(index)}>
-      {isMouse && <div className="mouse">
+      {isMouse && <div className="mouseh">
       <img className="Mouse-pawnh" src={mice} alt='gamemice'/>
         </div>}
     </div>
