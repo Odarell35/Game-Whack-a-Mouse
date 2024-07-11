85a1122c1b5bc0814617df53dbee50e1e59a9538
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2024-07-11 14:22:25 +0200

    moved files

---
+++ src/EasyMode.js
@@ -0,0 +1,167 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { addHighScore, getHighScores } from "./scorestorage";
+import Mouse from "./mouse-easy";
+import GameOverPopup from "./GameOverPopup"; // Import the GameOverPopup component
+import "./easy.css";
+import backArrow from "./Img/arrow-back.png";
+
+const EasyBoard = () => {
+  const [mice, setMice] = useState(Array(9).fill(false));
+  const [score, setScore] = useState(0);
+  const [highScores, setHighScores] = useState([]);
+  const [timeLeft, setTimeLeft] = useState(60);
+  const [gameOver, setGameOver] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+
+  let mouseInterval, timerInterval;
+
+  const navigate = useNavigate();
+  const handleClick1 = () => {
+    navigate("/GameMode");
+  };
+
+  const startGame = () => {
+    setScore(0);
+    setTimeLeft(60);
+    setGameOver(false);
+    setGameStarted(true);
+    setIsPaused(false);
+  };
+
+  const stopGame = () => {
+    clearInterval(mouseInterval);
+    clearInterval(timerInterval);
+    setGameStarted(false);
+    submitScore();
+    setIsPaused(true);
+    
+  };
+
+  const handleMouseClick = (index) => {
+    if (mice[index]) {
+      setScore(score + 1);
+      const newMice = [...mice];
+      newMice[index] = false;
+      setMice(newMice);
+    }
+  };
+
+  useEffect(() => {
+    if (gameStarted && !isPaused) {
+      mouseInterval = setInterval(() => {
+        const newMice = Array(9).fill(false);
+        const randomIndex = Math.floor(Math.random() * 9);
+        newMice[randomIndex] = true;
+        setMice(newMice);
+      }, 1000);
+
+      timerInterval = setInterval(() => {
+        setTimeLeft((prevTimeLeft) => {
+          if (prevTimeLeft <= 1) {
+            clearInterval(mouseInterval);
+            clearInterval(timerInterval);
+            setGameOver(true);
+            setGameStarted(false);
+            return 0;
+          }
+          return prevTimeLeft - 1;
+        });
+      }, 1000);
+
+      return () => {
+        clearInterval(mouseInterval);
+        clearInterval(timerInterval);
+      };
+    }
+  }, [gameStarted, isPaused]);
+
+  useEffect(() => {
+    getHighScores().then((scores) => setHighScores(scores));
+  }, []);
+
+  const submitScore = async () => {
+    await addHighScore("Player", score);
+    const updatedScores = await getHighScores();
+    updatedScores.push({ user: "Player", score });
+    updatedScores.sort((a, b) => b.score - a.score);
+    setHighScores(updatedScores);
+  };
+
+  const handleHome = () => {
+    navigate("/");
+  };
+
+  const handleReplay = () => {
+    setGameOver(false);
+    submitScore();
+    startGame();
+  };
+
+  return (
+    <div>
+      {gameOver && (
+        <GameOverPopup
+          score={score}
+          onReplay={handleReplay}
+          onHome={handleHome}
+        />
+      )}
+      <div className="container">
+        <header className="head">
+          <div className="level">
+            <div className="level-indicator">
+              <div className="level-text">Easy</div>
+              <div className="level-label">LEVEL</div>
+            </div>
+            <img
+              onClick={handleClick1}
+              src={backArrow}
+              alt="Back Arrow"
+              className="back-arrow-easy"
+            />
+          </div>
+          <div className="title">WHACK-A-MOUSE!!</div>
+          <div className="score-timer">
+            <span className="timerBar-easy">
+              <span
+                className="timer-easy"
+                style={{ width: `${(timeLeft / 60) * 100}%` }}
+              ></span>
+            </span>
+            <div className="high-score">
+              <span className="high-score-icon">&#127942;</span>
+              <span className="high-score-value">
+                HIGHSCORE: {highScores.length > 0 ? highScores[0].score : 0}
+              </span>
+            </div>
+          </div>
+        </header>
+        <section className="game-board">
+          <div className="game-area">
+            {mice.map((isMouse, index) => (
+              <Mouse
+                key={index}
+                index={index}
+                isMouse={isMouse}
+                handleMouseClick={handleMouseClick}
+              />
+            ))}
+          </div>
+          {!gameStarted ? (
+            <button className="start-button" onClick={startGame}>
+              Start
+            </button>
+          ) : (
+            <button className="start-button" onClick={stopGame}>
+              Pause
+            </button>
+          )}
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default EasyBoard;
---
+++ src/GameMode.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./GameMode.css";
+import "./medium.css";
+import "./Hardmode.css";
+import { useNavigate } from "react-router-dom";
+import mousehammer from "./Img/mouse-hammer.png";
+import backArrow from "./Img/arrow-back.png";
+
+function Home() {
+  const navigate = useNavigate();
+  const handleClick1 = () => {
+    navigate("/EasyMode");
+  };
+  const handleClick2 = () => {
+    navigate("/MediumMode");
+  };
+  const handleClick3 = () => {
+    navigate("/HardMode");
+  };
+  const handleClick4 = () => {
+    navigate("/");
+  };
+
+  return (
+    <div class="container">
+      <img src={mousehammer} onClick={handleClick4} alt="Mouse Character" class="mouse" />
+      <div class="buttons">
+        <button onClick={handleClick1} class="button red">
+          EASY
+        </button>
+        <button onClick={handleClick2} class="button pink">
+          MEDIUM
+        </button>
+        <button onClick={handleClick3} class="button blue">
+          HARD
+        </button>
+      </div>
+      <img
+        onClick={handleClick4}
+        src={backArrow}
+        alt="Back Arrow"
+        className="back-arrow"
+      />
+    </div>
+  );
+}
+
+export default Home;
